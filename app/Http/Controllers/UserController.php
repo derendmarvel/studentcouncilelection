4900889b86889be14638d898203f73a63e2c4de5
derendmarvel <derend101@gmail.com>
2024-01-22 11:26:58 +0700

    Sign In Error Handling Done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,24 +11,52 @@ class UserController extends Controller
     public function store(Request $request)
     {
         $validatedData = $request->validate([
-            'email' => 'required',
-            'nim' => 'required',
+            'email' => 'required|email',
+            'nim' => 'required|size:13',
         ]);
 
-        // if ($request->fails()) {
-        //     return redirect()->back()->withErrors(['email' => 'The email is already registered.']);
-        // }
-
         if($validatedData['email'] != 'bma@ciputra.ac.id'){
-            $user = User::create([
-                'email' => $validatedData['email'],
-                'nim' => $validatedData['nim'],
-                'role' => 2,
-            ]);
+            if (!str_ends_with($validatedData['email'], '@student.ciputra.ac.id')) {
+                return redirect()->back()->withErrors(['email' => 'Invalid email format. Please use an email ending with @student.ciputra.ac.id']);
+            }
+            $existingEmail = User::where('email', $validatedData['email'])->first();
+            $existingNIM = User::where('nim', $validatedData['nim'])->first();
 
-            Auth::login($user);
-            return redirect()->route('main');
-        }else{
+            if(!$existingEmail){
+                if(!$existingNIM){
+                    if(strlen($validatedData['nim']) === 13){
+                        $allowedPrefixes = ['010601', '010602', '010604', '020603', '020604', '020606', '030601', '040601', '040602', '040604', '050601', '050602', '060601', '060602','070601', '070602'];
+                        $startsWithAllowedPrefix = false;
+    
+                        foreach ($allowedPrefixes as $prefix) {
+                            if (str_starts_with($validatedData['nim'], $prefix)) {
+                                $startsWithAllowedPrefix = true;
+                                break;
+                            }
+                        }
+    
+                        if($startsWithAllowedPrefix){
+                            $user = User::create([
+                                'email' => $validatedData['email'],
+                                'nim' => $validatedData['nim'],
+                                'role' => 2,
+                            ]);
+                            
+                            Auth::login($user);
+                            return redirect()->route('main');
+                        } else {
+                            return redirect()->back()->withErrors(['nim' => 'Invalid NIM format.']);
+                        }
+                    } else {
+                        return redirect()->back()->withErrors(['nim' => 'Invalid NIM format.']);
+                    }
+                } else {
+                    return redirect()->back()->withErrors(['nim' => 'NIM has already been used to vote.']);
+                }
+            } else {
+                return redirect()->back()->withErrors(['email' => 'Email has already been used to vote.']);
+            }
+        } else {
             $user = User::where('email', 'bma@ciputra.ac.id')->first();
 
             Auth::login($user);
--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="card w-100 h-100 px-5 py-3 rounded-4" data-aos="fade-up" data-aos-duration="2000">
         <div class="card-body">
-            <h1 class = "heading-2 fw-bold"> LOGIN </h1>
+            <h1 class = "heading-2 fw-bold"> SIGN IN </h1>
                 <form method="POST" action="/registration">
                 @method('post')
                 @csrf
@@ -22,16 +22,16 @@
 
                 <div class="row mb-3">
                     <label for="nim" class="my-2">{{ __('NIM') }}</label>
-                    <input id="nim" type="text" class="form-control mb-3" name="nim" required>
+                    <input id="nim" type="text" class="form-control @error('nim') is-invalid @enderror" name="nim" required>
 
-                    <!-- @error('password')
+                    @error('nim')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
-                    @enderror -->
+                    @enderror
                 </div>
 
-                <div class="row my-3">
+                <div class="row mb-3 mt-4">
                     <button type="submit" class="btn btn-danger orange-div fw-bold">
                         {{ __('SIGN IN') }}
                     </button>
